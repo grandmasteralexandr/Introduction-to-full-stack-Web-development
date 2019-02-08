a2858f81771e0ac2ec3a1ebb90879e4d08dfcfec
aokunev <alexandr.okunev.uttc@gmail.com>
2019-02-08 17:13:01 +0200

    ps2 draft

--- WD_PS2/warm-up/script.js
+++ WD_PS2/warm-up/script.js
@@ -1,6 +1,11 @@
 /* Event listeners */
 const task1Button = document.getElementsByName("task1-button")[0];
+const task2ButtonToTime = document.getElementsByName("task2-button-to-time")[0];
+const task2ButtonToSec = document.getElementsByName("task2-button-to-sec")[0];
+
 task1Button.addEventListener("click", calculateSum);
+task2ButtonToTime.addEventListener("click", convertToTime);
+task2ButtonToSec.addEventListener("click", convertToSec);
 
 /* Task 1 */
 function calculateSum() {
@@ -34,3 +39,21 @@ function isInteger(number) {
     return number.match("^\\d+$");
 }
 
+/* Task 2 */
+function convertToTime() {
+    const form = document.getElementsByName("task2-form")[0];
+    const formInputs = form.getElementsByClassName("form__input");
+    let number = formInputs[0].value;
+
+    if (isInteger(number)) {
+
+    } else {
+        alert("Input must be a number");
+    }
+}
+
+function convertToSec() {
+    const form = document.getElementsByName("task2-form")[0];
+    const formInputs = form.getElementsByClassName("form__input");
+    let time = formInputs[1].value;
+}
