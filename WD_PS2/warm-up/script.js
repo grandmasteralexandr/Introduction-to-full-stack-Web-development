7367ad75c82820d39e7103c6e3f7f7de1247d2c0
aokunev <alexandr.okunev.uttc@gmail.com>
2019-02-08 10:42:28 +0200

    ps2 draft

---
+++ WD_PS2/warm-up/script.js
@@ -0,0 +1,9 @@
+/* Event listeners */
+let task1Button = document.getElementsByName("task1-button")[0];
+task1Button.addEventListener("click", calculateSum);
+
+/* Task 1 */
+
+function calculateSum() {
+    alert("test");
+}
