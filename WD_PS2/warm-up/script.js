e39c54248d9393fe1f8107ef0129f75c4d041261
aokunev <alexandr.okunev.uttc@gmail.com>
2019-02-08 11:45:47 +0200

    ps2 draft

--- WD_PS2/warm-up/script.js
+++ WD_PS2/warm-up/script.js
@@ -1,9 +1,23 @@
 /* Event listeners */
-let task1Button = document.getElementsByName("task1-button")[0];
+const task1Button = document.getElementsByName("task1-button")[0];
 task1Button.addEventListener("click", calculateSum);
 
 /* Task 1 */
-
 function calculateSum() {
-    alert("test");
+    if (checkInputs()) {
+        alert("test");
+    }
+
+    function checkInputs() {
+        const form = document.getElementsByName("task1-form")[0];
+        const formInputs = form.getElementsByClassName("form__input");
+        for (let i = 0; i < formInputs.length; i++) {
+            if (!Number.isInteger(formInputs[i].value)) {
+                alert("Input must be a number");
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
