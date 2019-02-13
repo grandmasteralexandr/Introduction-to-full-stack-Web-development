f29d2dfb469788191622cd18e35b383ef0bef7d3
aokunev <alexandr.okunev.uttc@gmail.com>
2019-02-13 10:42:22 +0200

    ps2 warm up

--- WD_PS2/warm-up/script.js
+++ WD_PS2/warm-up/script.js
@@ -57,9 +57,17 @@ function convertSecToTime() {
         let minutes = Math.trunc(timeInSec / 60) - hours * 60;
         let seconds = timeInSec - minutes * 60 - hours * 3600;
 
-        hours = hours < 10 ? "0" + hours : hours;
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
 
         alert(hours + ":" + minutes + ":" + seconds);
     } else {
