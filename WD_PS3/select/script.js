5947304c737203964f80b5f91e97c566861a49a6
aokunev <alexandr.okunev.uttc@gmail.com>
2019-02-22 15:48:51 +0200

    WD_PS3 add data

--- WD_PS3/select/script.js
+++ WD_PS3/select/script.js
@@ -9,6 +9,8 @@ const NAMES = [
 const WRAPPER = "<div class='wrapper'></div>";
 const DROPDOWN = "<ul class='dropdown'></ul>";
 const DROPDOWN_OPTION = "<li class='dropdown-option'></li>";
+const SELECT_DROPDOWN = ".dropdown";
+const SELECT_DROPDOWN_OPTIONS = ".dropdown-option";
 
 /* JQuery Code */
 $(document).ready(() => {
@@ -16,9 +18,14 @@ $(document).ready(() => {
     /* Add data */
     $("body").append(WRAPPER);
     $(".wrapper").append(DROPDOWN);
+    $(SELECT_DROPDOWN).append($(DROPDOWN_OPTION).text("Select friend"));
     for (let item of NAMES) {
-        $(".dropdown").append($(DROPDOWN_OPTION).text(item));
+        $(SELECT_DROPDOWN).append($(DROPDOWN_OPTION).text(item));
     }
 
+    $(SELECT_DROPDOWN_OPTIONS).hide();
 
+    $(SELECT_DROPDOWN).click(() => {
+        $(SELECT_DROPDOWN_OPTIONS).toggle();
+    });
 });
