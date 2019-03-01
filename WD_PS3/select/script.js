824869315a81f8cf000165ec1d074b1c528c96cf
alexandr <grandmasteralexandr@gmail.com>
2019-03-02 00:25:00 +0200

    WD_PS3 add stiles and hovers

--- WD_PS3/select/script.js
+++ WD_PS3/select/script.js
@@ -17,17 +17,16 @@ $(document).ready(() => {
     $("body").append(WRAPPER);
     $(".wrapper").append(DROPDOWN);
     const selectDropdown = $(".dropdown");
-    selectDropdown.append($(DROPDOWN_OPTION).text("Select friend").addClass("selected"));
 
     for (let item of NAMES) {
         selectDropdown.append($(DROPDOWN_OPTION).text(item));
     }
 
     const selectDropdownOptions = $(".dropdown-option");
+    selectDropdown.prepend($(DROPDOWN_OPTION).text("Select friend").addClass("selected default-option"));
     let selectedOption = $(".dropdown-option.selected");
-
-    selectDropdown.prepend(selectedOption.clone());
     let visibleOption = $(".dropdown li:first-child");
+    $(".dropdown li:last-child").addClass("last-option");
     selectDropdownOptions.hide();
 
     /* Handlers */
@@ -39,11 +38,21 @@ $(document).ready(() => {
         selectedOption.removeClass("selected");
         selectedOption = $(event.target);
         visibleOption.remove();
-        selectDropdown.prepend(selectedOption.clone());
+        selectDropdown.prepend(selectedOption.removeClass("hover").clone());
         visibleOption = $(".dropdown li:first-child");
         selectedOption.addClass("selected");
     });
 
+    selectDropdownOptions.hover(
+        (event) => {
+            $(event.target).addClass("hover");
+        },
+        (event) => {
+            $(event.target).removeClass("hover");
+        }
+    );
+
+    /* Close dropdown if click in another element */
     $(document).click((event) => {
         if (!selectDropdown.is(event.target) && selectDropdown.has(event.target).length === 0) {
             selectDropdownOptions.hide();
