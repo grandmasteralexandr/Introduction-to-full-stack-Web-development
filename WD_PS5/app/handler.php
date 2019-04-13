2efc2bdb802f3bb2873158bb955e899e0c7256bd
alexandr <grandmasteralexandr@gmail.com>
2019-04-13 14:41:26 +0300

    PS5 add error message

--- WD_PS5/app/handler.php
+++ WD_PS5/app/handler.php
@@ -1,5 +1,4 @@
 <?php
-require_once "config.php";
 require_once "DataBase.php";
 
 use shpp\wd\aokunev\DataBase;
--- WD_PS5/login.php
+++ WD_PS5/login.php
@@ -10,12 +10,16 @@ $title = 'Login';
 require_once 'app/layouts/header.php';
 ?>
 
-  <form action="app/handler.php" class="form login-form">
+  <form action="app/handler.php" method="post" class="form login-form">
     <label for="username" class="form__label">Enter your name</label>
-    <input type="email" class="form__input" name="email" id="email" placeholder="John Doe">
+    <?= isset($_SESSION['error']['username']) ? '<p class="error-message">' . $_SESSION['error']['username'] . '</p>' : '' ?>
+    <input type="text" class="form__input" name="username" id="username" placeholder="John Doe">
     <label for="pass" class="form__label">Enter your password</label>
+    <?= isset($_SESSION['error']['pass']) ? '<p class="error-message">' . $_SESSION['error']['pass'] . '</p>' : '' ?>
     <input type="password" class="form__input" name="pass" id="pass" placeholder="&bull;&bull;&bull;&bull;&bull;">
     <input type="submit" class="button" value="Submit">
     <div class="button-shadow"></div>
 
-<?php require_once 'app/layouts/footer.html';
+<?php
+require_once 'app/layouts/footer.html';
+unset($_SESSION['error']);
